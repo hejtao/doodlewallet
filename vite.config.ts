7c6820256c857617298f70dcc6abbba15b245ef1
Jiangtao He <hejtao@outlook.com>
2025-09-19 17:25:09 +0800

    vite single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSingleFile()
+  ],
   root: "src",
   resolve: {
     alias: {
